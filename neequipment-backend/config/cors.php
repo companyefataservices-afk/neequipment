e81ba1a445be000106fa28f7769b1b3c67834d5d
CelsoSebastiao <142792582+CelsoSebastiao@users.noreply.github.com>
2026-04-06 13:55:16 +0200

    Update cors.php

--- neequipment-backend/config/cors.php
+++ neequipment-backend/config/cors.php
@@ -3,10 +3,14 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://b2b.neequipment.co.mz',
+        'http://localhost:5173', // Para desenvolvimento local (Vite)
+        'http://localhost:3000'   // Alternativa local
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
